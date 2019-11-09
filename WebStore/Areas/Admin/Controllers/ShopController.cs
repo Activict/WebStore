697008d085b10b6d0990bf7899a985a921b81416
MaxPavlov <activict1@gmail.com>
2019-11-09 15:05:24 +0300

    Add Admin/Shop/Categories

---
+++ WebStore/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models.Data;
+using WebStore.Models.ViewModels.Shop;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop
+        public ActionResult Categories()
+        {
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories
+                                    .ToArray()
+                                    .OrderBy(m => m.Sorting)
+                                    .Select(m => new CategoryVM(m))
+                                    .ToList();
+            }
+            return View(categoryVMList);
+        }
+    }
+}
---
+++ WebStore/Models/ViewModels/Shop/CategoryVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebStore.Models.Data;
+
+namespace WebStore.Models.ViewModels.Shop
+{
+    public class CategoryVM
+    {
+        public CategoryVM()
+        {        }
+        public CategoryVM(CategoryDTO model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            Slug = model.Slug;
+            Sorting = model.Sorting;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+    }
+}
